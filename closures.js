78331856962ad0926c93b1faf05aa0e3e4bd1e4b
Kit Langton <kitlangton@gmail.com>
2016-03-15 12:29:34 -0400

    whee

--- closures.js
+++ closures.js
@@ -6,13 +6,27 @@ var assignments = {};
 ASSIGNMENT ONE: FIX THESE BUTTON LISTENERS VIA CLOSURE
 ********************************************* */
 
+/* Explanation
+
+   The on click method require a callback function. The problem
+   was that our initial anonymous function was holding on to a
+   reference to the iteration variable, which ended up being
+   4 when the callback was finally executed.
+
+   We needed to create a new closure around the value of i. So we
+   created a named function which returns an anonymous function
+   with the correct value of i imprisoned in a closure.
+
+   Hooray.
+
+ */
+
 assignments.one = function(){
 
   //There's a problem with this function
   var buttons = $('button');
 
   var message = function(arg){
-
     return function() {
       $('#clicked-btn').text('You clicked button #' + arg);
     }
@@ -39,8 +53,16 @@ assignments.one = function(){
 ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 ********************************************* */
 
-assignments.two = function(){
+The anonymous function passed to the browser's timing
+API is not called on the vikin object, but instead on
+the global 'window' object, thus this will be set to
+the 'window' object, which has no mood property.
 
+We need to store this is a variable (that) so that the
+anonymous function forms a closure around that. Closures
+are never formed around 'this.'
+
+assignments.two = function(){
   var viking = {  mood: undefined,
                   cheerUp: ( function() {
                           //This part works!
@@ -49,9 +71,10 @@ assignments.two = function(){
                           this.mood = "sad.";
                           $('#mood').text(this.mood);
 
+                          var that = this;
                           //So what goes wrong here?
                           setTimeout( (function() {
-                            this.mood = "Happy!";
+                            that.mood = "Happy!";
 
                             //THIS even runs correctly!
                             //What is UP with this? :(
