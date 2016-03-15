58c48759e34be090e4d41f86bffed0f2ab22ae9b
Sam O'Keefe <samok13@gmail.com>
2016-03-15 09:39:13 -0700

    playing around with this

--- closures.js
+++ closures.js
@@ -37,27 +37,30 @@ ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 assignments.two = function(){
 
   var viking = {  mood: undefined,
-                  cheerUp: ( function() {
+                  cheerUp: function() {
                           //This part works!
                           //Otherwise, it would be undefined
                           console.log('sad');
                           this.mood = "sad.";
                           $('#mood').text(this.mood);
-
+                          },
+                  setHappy: function(){
+                          var that = this;
                           //So what goes wrong here?
                           setTimeout( (function() {
-                            this.mood = "Happy!";
+                            that.mood = "Happy!";
 
                             //THIS even runs correctly!
                             //What is UP with this? :(
                             console.log("Cheered Up!")
-                          }), 1000);
-                      })
-           };
+                          }), 1000)
+                          }
+                };
 
 
 
   viking.cheerUp();
+  viking.setHappy();
 
   //waits an extra millisecond to make sure
   //that the other setTimeout has run.
