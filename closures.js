f972d30968431e0c81dbda4b3cf96bd9df2a0ce2
Sia <siakaramalegos@gmail.com>
2016-05-12 18:26:55 -0500

    Solves second problem

--- closures.js
+++ closures.js
@@ -33,24 +33,30 @@ ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 
 assignments.two = function(){
 
-  var viking = {  mood: undefined,
-                  cheerUp: ( function() {
-                          //This part works!
-                          //Otherwise, it would be undefined
-                          console.log('sad');
-                          this.mood = "sad.";
-                          $('#mood').text(this.mood);
+  var viking = {
+    mood: undefined,
+    changedMood: function(self){
+      setTimeout( (function() {
+        self.mood = "Happy!";
 
-                          //So what goes wrong here?
-                          setTimeout( (function() {
-                            this.mood = "Happy!";
+        //THIS even runs correctly!
+        //What is UP with this? :(
+        console.log("Cheered Up!")
+      }), 1000);
+    },
+    cheerUp: ( function() {
+      //This part works!
+      //Otherwise, it would be undefined
+      console.log('sad');
+      this.mood = "sad.";
+      $('#mood').text(this.mood);
 
-                            //THIS even runs correctly!
-                            //What is UP with this? :(
-                            console.log("Cheered Up!")
-                          }), 1000);
-                      })
-           };
+      //So what goes wrong here?
+      // setTimeout is performed on the window object which has a different scope ("this" points to window instead of viking).
+      this.changedMood(this);
+
+    })
+  };
 
 
 
