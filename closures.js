80d246cd6f2fb1869f414cc73764011cb13a93ac
Steven Chang <prime_pork@hotmail.com>
2016-07-23 09:17:06 +1000

    assignment two done

--- closures.js
+++ closures.js
@@ -36,25 +36,30 @@ ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 assignments.two = function(){
 
   var viking = {  mood: undefined,
-                  cheerUp: ( function() {
-                          //This part works!
-                          //Otherwise, it would be undefined
-                          console.log('sad');
-                          this.mood = "sad.";
-                          $('#mood').text(this.mood);
+                  // The facts
+                  // cheerUp is the results of the function inside running...
+                  // so if we took away the brackets, nothing would run
+                  cheerUp: function() {
+                             //This part works!
+                             //Otherwise, it would be undefined
+                             console.log('sad');
+                             this.mood = "sad.";
+                             $('#mood').text(this.mood);
 
-                          //So what goes wrong here?
-                          setTimeout( (function() {
-                            this.mood = "Happy!";
+                             //So what goes wrong here?
+                             // I think the problem is that setTimeout isn't assigned to anything so it's just being called by the window...
+                             // What we need is viking to call the setTimeout...
+                             (function(){ setTimeout( viking.changeMood(), 1000) })();
+                           },
 
-                            //THIS even runs correctly!
-                            //What is UP with this? :(
-                            console.log("Cheered Up!")
-                          }), 1000);
-                      })
-           };
+                  changeMood: function(){
+                    this.mood = "Happy!";
 
-
+                    //THIS even runs correctly!
+                    //What is UP with this? :(
+                    console.log("Cheered Up!")
+                  },
+  };
 
   viking.cheerUp();
 
