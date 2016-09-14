7bf2ceecacd99ca2a7e3dd3ff3f95f38a0c9e8a3
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-14 12:22:47 -0400

    finished assignment

--- closures.js
+++ closures.js
@@ -1,4 +1,3 @@
-
 var assignments = {};
 
 
@@ -6,21 +5,25 @@ var assignments = {};
 ASSIGNMENT ONE: FIX THESE BUTTON LISTENERS VIA CLOSURE
 ********************************************* */
 
-assignments.one = function(){
+assignments.one = function() {
 
   //There's a problem with this function
   var buttons = $('button');
 
   // No matter what I click, it always picks the same element
   // could it be CLOSURES???
+  var clicked = function(i) {
+    // i is preserved in the 'clicked' closure.
+    $(buttons[i]).on('click', function() {
+      $('#clicked-btn').text('You clicked button #' + i);
+    });
+  };
+
   for (var i = 0; i < buttons.length; i++) {
 
     // somehow, i is always the same value
-     $(buttons[i]).on('click', function() {
-        $('#clicked-btn').text('You clicked button #' + i);
-     });
+    clicked(i);
   }
-
 
 }
 
@@ -30,26 +33,28 @@ assignments.one = function(){
 ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 ********************************************* */
 
-assignments.two = function(){
+assignments.two = function() {
 
-  var viking = {  mood: undefined,
-                  cheerUp: ( function() {
-                          //This part works!
-                          //Otherwise, it would be undefined
-                          console.log('sad');
-                          this.mood = "sad.";
-                          $('#mood').text(this.mood);
+  var viking = {
+    mood: undefined,
+    cheerUp: (function() {
+      //This part works!
+      //Otherwise, it would be undefined
+      console.log('sad');
+      this.mood = "sad.";
+      $('#mood').text(this.mood);
 
-                          //So what goes wrong here?
-                          setTimeout( (function() {
-                            this.mood = "Happy!";
+      //So what goes wrong here?
+      setTimeout((function() {
+        // change the 'this' to the original parent.
+        viking.mood = "Happy!";
 
-                            //THIS even runs correctly!
-                            //What is UP with this? :(
-                            console.log("Cheered Up!")
-                          }), 1000);
-                      })
-           };
+        //THIS even runs correctly!
+        //What is UP with this? :(
+        console.log("Cheered Up!")
+      }), 1000);
+    })
+  };
 
 
 
@@ -58,7 +63,7 @@ assignments.two = function(){
   //waits an extra millisecond to make sure
   //that the other setTimeout has run.
   //The problem is NOT here
-  setTimeout( function() {
+  setTimeout(function() {
     $('#mood').text(viking.mood);
   }, 1001);
 
@@ -73,11 +78,9 @@ assignments.two = function(){
 
 
 
-
-
 // Don't touch this. Just the setup
 
-$(document).ready(function(){
+$(document).ready(function() {
 
   assignments.one();
   assignments.two();
