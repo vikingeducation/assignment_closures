0d6b049f6608218dfc6d115dc3870cade561d693
Deepak <grc.deepak@gmail.com>
2016-11-20 12:14:52 -0800

    closure case closed

--- closures.js
+++ closures.js
@@ -13,12 +13,19 @@ assignments.one = function(){
 
   // No matter what I click, it always picks the same element
   // could it be CLOSURES???
+  // Solution:
+  // All the clickCallback functions were referencing the same closure variable "i",
+  // Now, each clickCallback function will have its own closure variable.
+  var clickCallback = function(num) {
+    return function() {
+      $('#clicked-btn').text('You clicked button #' + num);
+    }
+  };
+
   for (var i = 0; i < buttons.length; i++) {
 
     // somehow, i is always the same value
-     $(buttons[i]).on('click', function() {
-        $('#clicked-btn').text('You clicked button #' + i);
-     });
+     $(buttons[i]).on('click', clickCallback(i));
   }
 
 
@@ -40,9 +47,11 @@ assignments.two = function(){
                           this.mood = "sad.";
                           $('#mood').text(this.mood);
 
-                          //So what goes wrong here?
+                          // So what goes wrong here?
+                          // Solution:
+                          // "this" was actully not viking, we need to call viking.mood
                           setTimeout( (function() {
-                            this.mood = "Happy!";
+                            viking.mood = "Happy!";
 
                             //THIS even runs correctly!
                             //What is UP with this? :(
