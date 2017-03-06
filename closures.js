8a6fc0c9117f7f88915169291059f08f9a33dba2
Kenny Kottenstette <kennykotten@gmail.com>
2017-03-05 20:18:24 -0800

    finish problem 1, begin numero dos

--- closures.js
+++ closures.js
@@ -5,6 +5,8 @@ var assignments = {};
 /* ********************************************
 ASSIGNMENT ONE: FIX THESE BUTTON LISTENERS VIA CLOSURE
 ********************************************* */
+/*
+The Original:
 
 assignments.one = function(){
 
@@ -23,12 +25,74 @@ assignments.one = function(){
 
 
 }
+*/
+
+assignments.one = function(){
+  var buttons = $('button');
+
+  var giveEachItsOwnClosure = function(n){
+     $(buttons[n]).on('click', function() {
+        console.log(this)
+        $('#clicked-btn').text('You clicked button #' + n);
+     });
+    return n;
+  }
+  for (var i = 0; i < buttons.length; i++) {
+    giveEachItsOwnClosure(i);
+  }
+}
+
+/*
+
+Each of the "children" jQuery functions all share the same reference - their parent's (assignment.one) function.
+The closures for these "children" jQuery functions aren't created until called. By then this, the referenced
+it's getting is the last "i value" used in the for loop.
+
+By creating the new function "giveEachItsOwnClosure," a new closure is created each time that function in run
+in the for loop.
+*/
 
 
 
 /* ********************************************
 ASSIGNMENT TWO: CHEER UP THE SAD VIKING VIA CLOSURE
 ********************************************* */
+/*The Original:
+assignments.two = function(){
+
+  var viking = {  mood: undefined,
+                  cheerUp: ( function() {
+                          //This part works!
+                          //Otherwise, it would be undefined
+                          console.log('sad');
+                          this.mood = "sad.";
+                          $('#mood').text(this.mood);
+
+                          //So what goes wrong here?
+                          setTimeout( (function() {
+                            this.mood = "Happy!";
+
+                            //THIS even runs correctly!
+                            //What is UP with this? :(
+                            console.log("Cheered Up!")
+                          }), 1000);
+                      })
+           };
+
+
+
+  viking.cheerUp();
+
+  //waits an extra millisecond to make sure
+  //that the other setTimeout has run.
+  //The problem is NOT here
+  setTimeout( function() {
+    $('#mood').text(viking.mood);
+  }, 1001);
+
+
+};*/
+
 
 assignments.two = function(){
 
